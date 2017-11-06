140a64fb058d0e6d954f81cf40180300a4793654
Tatsuya Yamamoto <tgx20ay.tov.boc@gmail.com>
2017-11-07 03:28:01 +0900

    fix #3 設定管理用クラス作成、設定変更用フォームへ対応

---
+++ ConvolutionalNeuralNetwork/Setting.vb
@@ -0,0 +1,7 @@
+﻿Public Class Setting
+    Public Shared SEED As Integer = 65535
+    Public Shared ERROR_LIMIT As Double = 0.001
+    Public Shared HIDDEN_NO As Integer = 3
+    Public Shared ALPHA As Double = 10
+    Public Shared MAX_CALCULATION As Integer = 500
+End Class
--- ConvolutionalNeuralNetwork/SettingForm.vb
+++ ConvolutionalNeuralNetwork/SettingForm.vb
@@ -1,9 +1,58 @@
 ﻿Public Class SettingForm
-    Private Sub SettingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private validMessage As String = ""
 
+    Private Sub SettingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextSeed.Text = Setting.SEED.ToString()
+        TextErrorLimit.Text = Setting.ERROR_LIMIT.ToString()
+        TextHiddenNum.Text = Setting.HIDDEN_NO.ToString()
+        TextAlpha.Text = Setting.ALPHA.ToString()
+        TextMaxCalculation.Text = Setting.MAX_CALCULATION.ToString()
     End Sub
 
+    Private Function CheckValidation() As Boolean
+        Dim isSuccess As Boolean = True
+        Dim tmpi As Integer
+        Dim tmpd As Double
+        If Integer.TryParse(TextSeed.Text, tmpi) Then
+            Setting.SEED = tmpi
+        Else
+            validMessage += "乱数シード" + vbCrLf
+            isSuccess = False
+        End If
+        If Double.TryParse(TextErrorLimit.Text, tmpd) Then
+            Setting.ERROR_LIMIT = tmpd
+        Else
+            validMessage += "誤差上限" + vbCrLf
+            isSuccess = False
+        End If
+        If Integer.TryParse(TextHiddenNum.Text, tmpi) Then
+            Setting.HIDDEN_NO = tmpi
+        Else
+            validMessage += "中間層のニューロン" + vbCrLf
+            isSuccess = False
+        End If
+        If Double.TryParse(TextAlpha.Text, tmpd) Then
+            Setting.ALPHA = tmpd
+        Else
+            validMessage += "学習係数" + vbCrLf
+            isSuccess = False
+        End If
+        If Integer.TryParse(TextMaxCalculation.Text, tmpi) Then
+            Setting.MAX_CALCULATION = tmpi
+        Else
+            validMessage += "最大計算回数" + vbCrLf
+            isSuccess = False
+        End If
+        Return isSuccess
+    End Function
+
     Private Sub ButtonConfirm_Click(sender As Object, e As EventArgs) Handles ButtonConfirm.Click
+        If Not CheckValidation() Then
+            MessageBox.Show(validMessage + vbCrLf +
+                            "の項目は入力値が不正のため更新されませんでした。",
+                            "警告", MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation)
+        End If
 
         Me.Close()
     End Sub
